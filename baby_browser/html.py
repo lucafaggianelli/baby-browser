c228ce3f6b361b8ced7e9633f7806c3ee0a219a9
Luca Faggianelli <luca.faggianelli@gmail.com>
2022-06-05 16:01:09 +0200

    Handle html ent and render only body

--- baby_browser/html.py
+++ baby_browser/html.py
@@ -1,10 +1,54 @@
+HTML_ENTITIES = {
+    'nbsb': ' ',
+    'lt': '<',
+    'gt': '>',
+    'amp': '&',
+    'quot': '"',
+    'apos': "'",
+    'cent': '¢',
+    'pound': '£',
+    'yen': '¥',
+    'euro': '€',
+    'copy': '©',
+    'reg': '®',
+}
+
+
+def _convert_html_entity(value: str):
+    return HTML_ENTITIES[value]
+
+
 def render_html(html: str):
     in_tag = False
+    in_tag_name = False
+    in_body = False
+    in_entity = False
+
+    tag_name = ""
+    entity_content = ""
 
     for c in html:
         if c == "<":
             in_tag = True
+            in_tag_name = True
+            tag_name = ""
         elif c == ">":
             in_tag = False
-        elif not in_tag:
+            in_tag_name = False
+
+            if tag_name == 'body':
+                in_body = True
+        elif not in_tag and c == '&':
+            in_entity = True
+            entity_content = ""
+        elif in_entity and c == ';':
+            in_entity = False
+            print(_convert_html_entity(entity_content), end="")
+        elif in_tag and in_tag_name and c == ' ':
+            in_tag_name = False
+        elif in_tag_name:
+            tag_name += c.lower()
+        elif in_entity:
+            entity_content += c.lower()
+        elif not in_tag and in_body:
             print(c, end="")
