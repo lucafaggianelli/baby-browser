30a71de4491285cf7a881ab725bf37a0548b8d4c
Luca Faggianelli <luca.faggianelli@gmail.com>
2023-08-14 13:57:07 +0200

    refactor layout into a class

--- baby_browser/browser.py
+++ baby_browser/browser.py
@@ -1,6 +1,7 @@
 import sys
 import tkinter
 from tkinter.font import Font
+from typing import Literal
 
 from baby_browser.html import Text, lexer
 from baby_browser.logger import get_logger
@@ -41,6 +42,58 @@ def _load_page_content(url: str):
     return html
 
 
+class Layout:
+    def __init__(self, tokens, width: float):
+        self.tokens = tokens
+        self.width = width
+
+        self.render()
+
+    def render(self):
+        self.display_list = []
+        self.cursor_x: float = HSTEP
+        self.cursor_y: float = VSTEP
+
+        self.weight: Literal["normal", "bold"] = "normal"
+        self.style: Literal["roman", "italic"] = "roman"
+        self.font_size = 16
+
+        for token in self.tokens:
+            self._render_token(token)
+
+    def _render_token(self, token):
+        if isinstance(token, Text):
+            for word in token.text.split():
+                self._render_word(word)
+        elif token.tag in ("i", "em"):
+            self.style = "italic"
+        elif token.tag in ("/i", "/em"):
+            self.style = "roman"
+        elif token.tag in ("b", "strong"):
+            self.weight = "bold"
+        elif token.tag in ("/b", "/strong"):
+            self.weight = "normal"
+
+    def _render_word(self, word: str):
+        font = Font(
+            family="Times New Roman",
+            size=self.font_size,
+            weight=self.weight,
+            slant=self.style,
+        )
+
+        word_width = font.measure(word)
+
+        if self.cursor_x + word_width > self.width - HSTEP:
+            # Break line on word
+            self.cursor_y += font.metrics("linespace") * 1.25
+            self.cursor_x = HSTEP
+
+        self.display_list.append((self.cursor_x, self.cursor_y, word, font))
+
+        self.cursor_x += word_width + font.measure(" ")
+
+
 class Browser:
     def __init__(self):
         self.window = tkinter.Tk()
@@ -59,7 +112,9 @@ class Browser:
         self.window.bind("<MouseWheel>", self._on_scroll)
 
     def _on_resize(self, event: tkinter.Event):
-        self._layout(self.loaded_text)
+        self.layout.width = self.canvas.winfo_width()
+        self.layout.render()
+
         self.canvas.delete("all")
         self.draw()
 
@@ -82,50 +137,20 @@ class Browser:
         self.canvas.delete("all")
         self.draw()
 
-    def _layout(self, tokens: list):
-        self.display_list = []
-        cursor_x, cursor_y = HSTEP, VSTEP
-
-        weight = "normal"
-        style = "roman"
-
-        for token in tokens:
-            if isinstance(token, Text):
-                for word in token.text.split():
-                    font = Font(
-                        family="Times New Roman", size=16, weight=weight, slant=style
-                    )
-
-                    word_width = font.measure(word)
-
-                    if cursor_x + word_width > self.canvas.winfo_width() - HSTEP:
-                        # Break line on word
-                        cursor_y += font.metrics("linespace") * 1.25
-                        cursor_x = HSTEP
-
-                    self.display_list.append((cursor_x, cursor_y, word, font))
-
-                    cursor_x += word_width + font.measure(" ")
-            elif token.tag in ("i", "em"):
-                style = "italic"
-            elif token.tag in ("/i", "/em"):
-                style = "roman"
-            elif token.tag in ("b", "strong"):
-                weight = "bold"
-            elif token.tag in ("/b", "/strong"):
-                weight = "normal"
-
     def load_page(self, url: str):
         html = _load_page_content(url)
 
-        self.loaded_text = lexer(html)
+        self.tokens = lexer(html)
 
-        self._layout(self.loaded_text)
+        self.layout = Layout(self.tokens, self.canvas.winfo_width())
+
         self.draw()
 
     def draw(self):
-        for x, y, c, font in self.display_list:
-            is_before_viewport = y > self.scroll + self.canvas.winfo_height()
+        height = self.canvas.winfo_height()
+
+        for x, y, c, font in self.layout.display_list:
+            is_before_viewport = y > self.scroll + height
             is_after_viewport = y + VSTEP < self.scroll
 
             # If the text is not visible skip the rendering
